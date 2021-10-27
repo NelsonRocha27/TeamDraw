f352d0ba96fab10a99dab5539a324d5b1597e696
Nelson <nelsonrocha2727@gmail.com>
2021-10-27 16:28:20 +0100

    Add resources to the project Add grid definitions to layout

--- TeamDraw/MainWindow.xaml.cs
+++ TeamDraw/MainWindow.xaml.cs
@@ -15,14 +15,21 @@ using System.Windows.Shapes;
 
 namespace TeamDraw
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+         ChangeBackground("Resources/aptiv.jpg");
+      }
+
+      private void ChangeBackground(string resource)
+      {
+         ImageBrush b = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), resource)));
+         appGrid.Background = b;
+      }
+   }
 }
