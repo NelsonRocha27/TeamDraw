ccaf89ec66190fb385c42c8237ba0c1acae7b28e
Nelson <nelsonrocha2727@gmail.com>
2021-10-28 10:37:27 +0100

    Adds Style class for theme selection

--- TeamDraw/Data.cs
+++ TeamDraw/Data.cs
@@ -1,13 +1,14 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace TeamDraw
 {
-   enum Theme
+   public enum Theme
    {
       CHAMPIONS,
       APTIV,
@@ -30,22 +31,64 @@ namespace TeamDraw
       }
    }
 
-   class Style
+   public class Style
    {
-   }
-}
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+      public Brush textBoxBackground;
+      public Brush textBoxForeground;
+      public Brush labelBackground;
+      public int textBoxFontSize;
+      public int labelFontSize;
+      public string background;
+      public string music;
 
-namespace TeamDraw
-{
-   class Data
-   {
-      List<string> players;
+      public Style()
+      {
+         this.textBoxBackground = Brushes.Black;
+         this.textBoxForeground = Brushes.White;
+         this.labelBackground = Brushes.Black;
+         this.textBoxFontSize = 12;
+         this.labelFontSize = 12;
+         this.background = "";
+         this.music = "";
+      }
+
+      public Style(Theme theme)
+      {
+         if (Theme.CHAMPIONS == theme)
+         {
+            var converter = new BrushConverter();
+            var brush = (Brush)converter.ConvertFromString("#A5091442");
+            this.textBoxBackground = brush;
+            this.textBoxForeground = Brushes.White;
+            brush = (Brush)converter.ConvertFromString("#FF85BAEC");
+            this.labelBackground = brush;
+            this.textBoxFontSize = 20;
+            this.labelFontSize = 32;
+            this.background = "Resources/champions2.jpg";
+            this.music = "";
+
+            
+         }
+         else if (Theme.APTIV == theme)
+         {
+            this.textBoxBackground = Brushes.Black;
+            this.textBoxForeground = Brushes.White;
+            this.labelBackground = Brushes.OrangeRed;
+            this.textBoxFontSize = 20;
+            this.labelFontSize = 32;
+            this.background = "Resources/aptiv.jpg";
+            this.music = "";
+         }
+         else
+         {
+            this.textBoxBackground = Brushes.Black;
+            this.textBoxForeground = Brushes.White;
+            this.labelBackground = Brushes.Black;
+            this.textBoxFontSize = 12;
+            this.labelFontSize = 12;
+            this.background = "";
+            this.music = "";
+         }
+      }
    }
 }
->>>>>>> 3291871ac75317cc7ac49b90297f8f9a84d89f10
